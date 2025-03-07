b72c61651ca26b6b579a52bd092ef229e7a63e88
Pallavi Patil <pallavishravan545@gmail.com>
2025-03-07 23:49:15 +0530

    User Data Add & Update & Delete in FireStore

--- lib/Users/user_page.dart
+++ lib/Users/user_page.dart
@@ -1,47 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fire_cli/Users/user_page.dart';
-import 'package:flutter_fire_cli/note_model.dart';
+import 'package:flutter_fire_cli/Users/user_model.dart';
 
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: UserPage()
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<UserPage> createState() => _UserPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _UserPageState extends State<UserPage> {
   late FirebaseFirestore db;
-  var titleController = TextEditingController();
-  var descController = TextEditingController();
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var ageController = TextEditingController();
 
   @override
   void initState() {
@@ -49,15 +21,16 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     db = FirebaseFirestore.instance;
   }
-
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.blue.shade100,centerTitle: true,
-        title: Text("Notes App"),
+        title: Text("User App"),
       ),
       body: StreamBuilder(
-          stream: db.collection("notes").snapshots(),
+          stream: db.collection("users").snapshots(),
           // future: db.collection("notes").get(),
           builder: (ctx, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -68,21 +41,21 @@ class _HomePageState extends State<HomePage> {
               return ListView.builder(
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (ctx, index) {
-                    var model =
-                        NoteModel.fromJson(snapshot.data!.docs[index].data());
+                    var model = UserModel.fromJson(snapshot.data!.docs[index].data());
                     model.id= snapshot.data!.docs[index].id!;
 
                     return InkWell(
                       onTap: () {
-                        titleController.text = model.title!;
-                        descController.text = model.body!;
+                        nameController.text = model.name!;
+                        emailController.text = model.email!;
+                        ageController.text = model.age.toString();
 
                         showModalBottomSheet(
                             context: context,
                             builder: (ctx) {
                               return Container(
                                 height:
-                                    MediaQuery.of(context).viewInsets.bottom == 0.0 ? 400 : 800,
+                                MediaQuery.of(context).viewInsets.bottom == 0.0 ? 400 : 800,
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(5),
                                     color: Colors.lightBlueAccent.shade100
@@ -91,21 +64,21 @@ class _HomePageState extends State<HomePage> {
                                   padding: const EdgeInsets.all(20.0),
                                   child: Column(
                                     mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
+                                    MainAxisAlignment.spaceAround,
                                     children: [
-                                      Text("Update Note"),
+                                      Text("Update User"),
                                       SizedBox(
                                         height: 20,
                                       ),
                                       TextField(
                                         decoration: InputDecoration(
-                                          border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
-                                          ),
-                                          hintText: "Enter Title",
-                                          label: Text("Title")
+                                            border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
+                                            ),
+                                            hintText: "Enter Name",
+                                            label: Text("Name")
                                         ),
 
-                                        controller: titleController,
+                                        controller: nameController,
                                       ),
                                       SizedBox(
                                         height: 10,
@@ -114,35 +87,48 @@ class _HomePageState extends State<HomePage> {
                                         decoration: InputDecoration(
                                             border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
                                             ),
-                                            hintText: "Enter Desc",
-                                            label: Text("Desc")
+                                            hintText: "Enter Email",
+                                            label: Text("Email")
                                         ),
-                                        controller: descController,
+                                        controller: emailController,
+                                      ),
+                                      SizedBox(
+                                        height: 30,
+                                      ),
+                                      TextField(
+                                        decoration: InputDecoration(
+                                            border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
+                                            ),
+                                            hintText: "Enter Age",
+                                            label: Text("Age")
+                                        ),
+                                        controller: ageController,
                                       ),
                                       SizedBox(
                                         height: 30,
                                       ),
                                       ElevatedButton(
                                           onPressed: () {
-                                            var mMTitle =
-                                                titleController.text.toString();
-                                            var mDDesc =
-                                                descController.text.toString();
+                                            var mNName = nameController.text.toString();
+                                            var mEEmail = emailController.text.toString();
+                                            var mAAge = int.parse(ageController.text.toString());
 
-                                            db.collection("notes").doc(model.id).set(NoteModel(
-                                              title:mMTitle ,
-                                              body: mDDesc
+                                            db.collection("users").doc(model.id).set(UserModel(
+                                               name: mNName,
+                                              email:mEEmail ,
+                                              age: mAAge
                                             ).toJson()
                                             );
 
-                                           /* db.collection("notes").doc(model.id).update(NoteModel(
+                                            /* db.collection("notes").doc(model.id).update(NoteModel(
                                               title:mMTitle ,
                                               body:mDDesc ,
                                             ).toJson());*/
 
 
-                                            titleController.clear();
-                                            descController.clear();
+                                            nameController.clear();
+                                            emailController.clear();
+                                            ageController.clear();
                                             Navigator.pop(context);
 
                                           },
@@ -155,11 +141,18 @@ class _HomePageState extends State<HomePage> {
                       },
                       child: ListTile(
                         leading: Text("${index + 1}"),
-                        title: Text(model.title!),
-                        subtitle: Text(model.body!),
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("${model.name}"),
+                            SizedBox(width: 10,),
+                            Text("Age: ${model.age}"),
+                          ],
+                        ),
+                        subtitle: Text("${model.email}"),
                         trailing: InkWell(
                             onTap: () {
-                              db.collection("notes").doc(model.id).delete();
+                              db.collection("users").doc(model.id).delete();
                             },
                             child: Icon(Icons.delete)),
                       ),
@@ -184,7 +177,7 @@ class _HomePageState extends State<HomePage> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Text("Add Note"),
+                        Text("Add User"),
                         SizedBox(
                           height: 20,
                         ),
@@ -192,10 +185,10 @@ class _HomePageState extends State<HomePage> {
                           decoration: InputDecoration(
                               border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
                               ),
-                              hintText: "Enter Title",
-                              label: Text("Title")
+                              hintText: "Enter Name",
+                              label: Text("Name")
                           ),
-                          controller: titleController,
+                          controller: nameController,
                         ),
                         SizedBox(
                           height: 10,
@@ -204,28 +197,42 @@ class _HomePageState extends State<HomePage> {
                           decoration: InputDecoration(
                               border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
                               ),
-                              hintText: "Enter Desc",
-                              label: Text("Desc")
+                              hintText: "Enter Email",
+                              label: Text("Email")
                           ),
-                          controller: descController,
+                          controller: emailController,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(borderRadius:BorderRadius.circular(5),
+                              ),
+                              hintText: "Enter Age",
+                              label: Text("Age")
+                          ),
+                          controller: ageController,
                         ),
                         SizedBox(
                           height: 30,
                         ),
                         ElevatedButton(
                             onPressed: () {
-                              var mTitle = titleController.text.toString();
-                              var mDesc = descController.text.toString();
+                              var mName = nameController.text.toString();
+                              var mEmail = emailController.text.toString();
+                              var mAge = int.parse(ageController.text.toString());
 
                               db
-                                  .collection("notes")
-                                  .add(NoteModel(title: mTitle, body: mDesc)
-                                      .toJson())
+                                  .collection("users")
+                                  .add(UserModel(name: mName,email: mEmail,age: mAge)
+                                  .toJson())
                                   .then((value) {
                                 print("ID:=> ${value.id}");
                               });
-                              titleController.clear();
-                              descController.clear();
+                              nameController.clear();
+                              emailController.clear();
+                              ageController.clear();
                               Navigator.pop(context);
 
                             },
